039d4a9659fb8de1de7131e78425e903a57b4984
sandelam <59333218+sandelam@users.noreply.github.com>
2019-12-29 14:18:43 +0200

    Update CustomUserDetailsService.java

--- src/main/java/sec/project/config/CustomUserDetailsService.java
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -9,28 +9,31 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import sec.project.repository.AccountRepository;
+import sec.project.domain.Account;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private Map<String, String> accountDetails;
+    private AccountRepository accountrepository;
 
     @PostConstruct
     public void init() {
-        // this data would typically be retrieved from a database
-        this.accountDetails = new TreeMap<>();
-        this.accountDetails.put("ted", "$2a$06$rtacOjuBuSlhnqMO2GKxW.Bs8J6KI0kYjw/gtF0bfErYgFyNTZRDm");
+        Account account = new Account();
+        account.setPassword("password");
+        account.setUsername("username");
+        accountrepository.save(account);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!this.accountDetails.containsKey(username)) {
+        Account account = accountrepository.findByUsername(username);
+        if (account == null) {
             throw new UsernameNotFoundException("No such user: " + username);
         }
-
         return new org.springframework.security.core.userdetails.User(
-                username,
-                this.accountDetails.get(username),
+                account.getUsername(),
+                account.getPassword(),
                 true,
                 true,
                 true,
